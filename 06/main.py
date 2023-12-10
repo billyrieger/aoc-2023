b7ae51651a662f117feedde4cddb46f02b7f3e1d
Billy Rieger <wrieger@protonmail.com>
2023-12-10 17:44:34 -0500

    Day 6 improvement

--- 06/main.py
+++ 06/main.py
@@ -1,8 +1,16 @@
-import math, sys
+from math import sqrt, floor, ceil
+import sys
 
 def ways_to_win(time: int, record: int) -> int:
-    ways = [i for i in range(time) if i * (time - i) > record]
-    return len(ways)
+    # The record is broken if (time pressing button) * (time left in race) > record.
+    # Mathematically, this is solving the quadratic inequality x * (t - x) > r.
+    # x^2 - t*x + r < 0, use the quadratic formula and round carefully.
+    discriminant = time * time - 4 * record
+    min_exact = (time - sqrt(discriminant)) / 2
+    max_exact = (time + sqrt(discriminant)) / 2
+    min_win = max(floor(min_exact + 1), 0)
+    max_win = min(ceil(max_exact - 1), time)
+    return max_win - min_win + 1
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r') as f:
